bb925a7547cd819cbfc7824f0f5a53c83b17d244
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-04-30 15:57:02 +0530

    added routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './Pages/Home';
+import Category from './Pages/Category';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path='/Category' element={<Category/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
