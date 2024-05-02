6620a2a2b08a2c6736fa9152ab7970663a1ba0ec
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-05-02 14:46:28 +0530

    fixed ui errors

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,14 +7,13 @@ function Home() {
   return (
     <>
     <div className="home">
-    <div className='halfpart1' style={{position: 'relative'}}>
+      <div className='halfpart1' style={{position: 'relative'}}>
         <img src={bgimg} alt="Background" style={{width: '100%', height: '100%'}} />
         <div className="text-overlay">
           <span style={{color: 'white',fontSize:'1.2em',textAlign:'start'}}><h1>Discover new things on <br/> Superapp</h1></span>
         </div>
       </div>
-      <div className='halfpart2'><Registration/></div>
-      
+      <div className='halfpart2' style={{ overflowY: 'auto', maxHeight: '100vh' }}><Registration/></div>
     </div>
     </>
   )
